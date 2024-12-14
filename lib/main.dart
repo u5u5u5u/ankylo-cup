7c980036035c9458d5299c3518e013665404d9a0
IsseTeruhi-uni <isse.teruhi220@mail.kyutech.jp>
2024-12-15 02:17:49 +0900

    fix:conflict修正の間違い

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
