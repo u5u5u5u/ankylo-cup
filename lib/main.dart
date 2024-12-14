154eefe7bb69b9112cf34dd0ebfbc66605a23f51
U5 <kyutech.joko@gmail.com>
2024-12-14 11:24:24 +0900

    app check

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:ankylo_cup/presentation/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart'; // Add this line
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
 void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.debug,
   );
   runApp(const MiniGamesApp());
 }
